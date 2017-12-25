eab6dfd521627ef10c4fd8e6f9e7ef1009ecc8b6
ameerassafli <amiralassafi@yahoo.com>
2017-12-26 02:47:52 +0300

    Second Commit

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require ('express');
-const app = express();
-const path = require('path');
-const bodyParser = require('body-parser');
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : false }))
-app.use( express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 let items = [{
       imgurl:'https://www.nutella.com/nutella-gl-theme/images/custom/Nutella.png',
       name : 'Nutella' ,
@@ -21,5 +22,18 @@ app.post ('/api/items' , (req , res)=>{
 		price: req.body.price});
 		res.status(200).send('\n A new item has been added');
 })
-var listener = app.listen (process.env.PORT || 3000, function(){
-console.log(`Example app listening on port ${listener.address().port}!`)})
+io.on('connection', function(socket) {
+    
+    // socket.emit('news', {
+    //     hello: 'world'
+    // });
+
+    socket.on('my other event', function(data) {
+        console.log(data);
+    });
+});
+
+
+let listener = server.listen(process.env.PORT || 3100, function() {
+    console.log(`listeing on port ${listener.address().port}`)
+});
